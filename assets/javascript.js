56520993122e23b90caf6123c0c6a3aafa36fea1
Sempanha123 <sempanha02@gmail.com>
2024-06-18 14:30:49 +0700

    update

--- assets/javascript.js
+++ assets/javascript.js
@@ -416,7 +416,7 @@ $(document).ready(function(){
                             <img class="cursor-pointer" onclick="detail(24)" class="object-cover w-full h-fit mt-2" src="./assets/images/cach-choi-tai-xiu-2-600x400.jpg" alt=""></a>
                         </div>
                         <div>
-                            <p class="cursor-pointer" onclick="detail(24)" class="text-blue-700 hover:text-gray-800 duration-300"><a >Tài xỉu là trò chơi gì? Cách chơi tài xỉu tại Luke79.top</a></p>
+                            <p class="cursor-pointer" onclick="detail(24)" class="text-blue-700 hover:text-gray-800 duration-300"><a >Tài xỉu là trò chơi gì? Cách chơi tài xỉu tại hayppyluck.xyz</a></p>
                             <p class="cursor-pointer" onclick="detail(24)" class="flex items-center gap-2">
                                 <svg class="h-6 w-6 text-gray-700"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="4" y="5" width="16" height="16" rx="2" />  <line x1="16" y1="3" x2="16" y2="7" />  <line x1="8" y1="3" x2="8" y2="7" />  <line x1="4" y1="11" x2="20" y2="11" />  <line x1="11" y1="15" x2="12" y2="15" />  <line x1="12" y1="15" x2="12" y2="18" /></svg>
                                 <span class="text-xl">11 Tháng Tám, 2023</span>
@@ -429,7 +429,7 @@ $(document).ready(function(){
                             <img class="cursor-pointer" onclick="detail(25)" class="object-cover w-full h-fit mt-2" src="./assets/images/cach-choi-lien-minh-huyen-thoai-len-do-600x400.jpg" alt=""></a>
                         </div>
                         <div>
-                            <p class="cursor-pointer" onclick="detail(25)" class="text-blue-700 hover:text-gray-800 duration-300"><a >Cách chơi Liên Minh Huyền Thoại hiệu quả tại Luke79.top </a></p>
+                            <p class="cursor-pointer" onclick="detail(25)" class="text-blue-700 hover:text-gray-800 duration-300"><a >Cách chơi Liên Minh Huyền Thoại hiệu quả tại hayppyluck.xyz </a></p>
                             <p class="cursor-pointer" onclick="detail(25)" class="flex items-center gap-2">
                                 <svg class="h-6 w-6 text-gray-700"  width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">  <path stroke="none" d="M0 0h24v24H0z"/>  <rect x="4" y="5" width="16" height="16" rx="2" />  <line x1="16" y1="3" x2="16" y2="7" />  <line x1="8" y1="3" x2="8" y2="7" />  <line x1="4" y1="11" x2="20" y2="11" />  <line x1="11" y1="15" x2="12" y2="15" />  <line x1="12" y1="15" x2="12" y2="18" /></svg>
                                 <span class="text-xl">31 Tháng Năm, 2023</span>
